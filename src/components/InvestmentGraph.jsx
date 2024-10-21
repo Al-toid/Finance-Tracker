8976e00d30c5c7669cc262f97a7e663435d65f97
marenas1 <140763896+marenas1@users.noreply.github.com>
2024-10-21 14:39:43 -0400

    Investment Graph Implemented
    Using user inputs the investment graph will dynamically change. Later a form will be implemented to make it a little neater in regards to the user inputs

---
+++ src/components/InvestmentGraph.jsx
@@ -0,0 +1,79 @@
+import {Line} from "react-chartjs-2"
+import PropTypes from "prop-types"
+import { Chart as ChartJs,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend 
+} from 'chart.js' 
+
+ChartJs.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend 
+)
+LineGraph.propTypes = {
+    option: PropTypes.object,
+    years: PropTypes.number,
+    monthlyInvestment: PropTypes.number,
+    rate: PropTypes.number
+}
+
+
+function LineGraph(props){
+    //using the monthly investment amount, we create an array with the sum of these investments
+    let regularSavings=[]
+    let years=[]
+    let growthRate=1+(props.rate/100)
+    for(let i=0;i<props.years;i++){
+        years[i]=2025+i
+        if (i==0){
+            regularSavings[i]=props.monthlyInvestment*12
+        }
+        else{
+            regularSavings[i]=regularSavings[i-1]+props.monthlyInvestment*12
+        }
+    }
+    //using compound interest we calculate growth over time
+    let investmentSavings=[]
+    for(let i=0;i<props.years;i++){
+        if (i==0){
+            investmentSavings[i]=(props.monthlyInvestment*12)*growthRate
+        }
+        else{
+            investmentSavings[i]=(investmentSavings[i-1]+props.monthlyInvestment*12)*growthRate
+        }
+    }
+    
+
+
+    let investmentData= {
+        //x axis
+        labels: years,
+        //y axis
+        datasets: [
+            {
+                label: "Potential",
+                data: investmentSavings,
+                borderColor: "green"   
+            },
+            {
+                label: "Regular Saving",
+                data: regularSavings,
+                borderColor: "gray"
+            },
+        ],
+    };
+    return(<Line options={props.option} data={investmentData} className='bg-green-100'></Line>)
+}
+
+
+
+export default LineGraph
--- src/pages/InvestmentsPage.jsx
+++ src/pages/InvestmentsPage.jsx
@@ -1,10 +1,61 @@
 import Navbar from '../components/Navbar'
-import LineGraph from '../components/LineGraph'
+import InvestmentGraph from '../components/InvestmentGraph'
+import RiskChart from '../components/RiskChart'
+import { useState } from 'react'
+
+
+
 function InvestmentsPage(){
+    const [budget,setBudget] = useState(150)
+    const [years,setYears] = useState(5)
+    const [growthRate, setGrowthRate]= useState(5)
+    const changeBudget = event =>{
+        setBudget(event.target.value)
+
+    }
+    const changeYears = event =>{
+        setYears(event.target.value)
+
+    }
+
+    const changeGrowthRate = event =>{
+        setGrowthRate(event.target.value)
+    }
+    let graphOptions= {
+        scales:{
+            y:{
+                min:0,
+            }
+        }
+    }
+    
     return (<>
+    <div className='bg-green-600 h-lvh'>
     <Navbar></Navbar>
-    <div className='w-8/12 m-auto'>
-        <LineGraph></LineGraph>
+    <br></br>
+    <p className='pl-6'>Fields here will be replaced with a form in the future</p>
+    <br></br>
+    <div className='flex w-2/4 justify-around'>
+    
+        <p>Input Monthly Budget</p>
+        <input onChange={changeBudget} value={budget} className='bg-green-500 w-12 text-center'></input>
+
+        <p>Input Years</p>
+        <input onChange={changeYears} value={years} className='bg-green-500 w-6 text-center'></input>
+
+        <p>Input Growth Rate</p>
+        <input onChange={changeGrowthRate} value={growthRate} className='bg-green-500 w-6 text-center'></input>
+    </div>
+    <br></br>
+    
+    <div className='flex'>
+      <div className='relative w-[75vw] h-[50vh]'>
+        <InvestmentGraph option={[graphOptions]} years={years} monthlyInvestment={budget} rate={growthRate}></InvestmentGraph>
+      </div>
+      <div className='relative w-[35vw] h-[50vh]'>
+        <RiskChart></RiskChart>
+      </div>
+    </div>
     </div>
     </>)
 }
